2d65cd99ee43cc4faeabb0ca6bd98d253f8b6d86
Flxke15 <105930630+Flxke15@users.noreply.github.com>
2022-08-19 12:00:13 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+   const divStyle = {
+    color : 'red',
+    backgroundColor : 'powderblue',
+    fontSize : 'larger',
+    padding : '10px',
+    margin : '30px'
+   }
+
+   return <div style={divStyle}>Hello React</div>
+}
+
+export default App;
